31a03992882a16c09a19115ab9f263e272cfc21f
Adam Khashiev <adamkhashiev2004@mail.ru>
2024-07-18 19:02:26 +0300

    some fixes

--- src/components/task-list/task-list.js
+++ src/components/task-list/task-list.js
@@ -1,44 +1,21 @@
 import React from 'react';
 import './task-list.css';
 
+import Task from '../task/task';
+
 export default function TaskList() {
   return (
     <ul className="todo-list">
         <li className="completed">
-        <div className="view">
-            <input className="toggle" type="checkbox"/>
-            <label>
-            <span className="description">Completed task</span>
-            <span className="created">created 17 seconds ago</span>
-            </label>
-            <button className="icon icon-edit"></button>
-            <button className="icon icon-destroy"></button>
-        </div>
+            <Task label='Completed Task' status='completed' created = {new Date()} />
         </li>
 
         <li className="editing">
-        <div className="view">
-            <input className="toggle" type="checkbox"/>
-            <label>
-            <span className="description">Editing task</span>
-            <span className="created">created 5 minutes ago</span>
-            </label>
-            <button className="icon icon-edit"></button>
-            <button className="icon icon-destroy"></button>
-        </div>
-        <input type="text" className="edit" value="Editing task"/>
+            <Task label='Editing task' onEdit={true} created = {new Date()} />
         </li>
-        
+
         <li>
-        <div className="view">
-            <input className="toggle" type="checkbox"/>
-            <label>
-            <span className="description">Active task</span>
-            <span className="created">created 5 minutes ago</span>
-            </label>
-            <button className="icon icon-edit"></button>
-            <button className="icon icon-destroy"></button>
-        </div>
+            <Task label='Active Task' created = {new Date()} />
         </li>
     </ul>
   );
---
+++ src/components/task/task.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './task.css';
+
+import { formatDistanceToNow } from 'date-fns'
+
+export default function Task({ label, status='', onEdit=false, created }) {
+    const timeAgo = formatDistanceToNow(created, { includeSeconds: true });
+    const inputEdit = onEdit ? <input  type="text" className="edit" value={label} /> : '';
+  
+    return (
+      <>
+      <div className="view">
+        <input className="toggle" type="checkbox" checked={status} />
+        <label htmlFor="username">
+          <span id="username" className="description">
+            {label}
+          </span>
+          <span className="created">created {timeAgo} ago</span>
+        </label>
+        <button type="button" className="icon icon-edit" aria-label="edit" />
+        <button type="button" className="icon icon-destroy" aria-label="destroy" />
+      </div>
+      
+      {inputEdit}
+      </>
+    )
+  }
