b79f9e66f450f486bd4557ecb17d7c0a949a3914
Adam Khashiev <adamkhashiev2004@mail.ru>
2024-07-18 15:42:22 +0300

    1.3.13

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './app.css'
 import NewTaskForm from '../new-task-form'
+import TaskList from '../task-list';
+import Footer from '../footer';
 
 const App = () => {
   return (
     <section className='todoapp'>
       <NewTaskForm />
+      <section className='main'>
+        <TaskList />
+        <Footer />
+      </section>
     </section>
   );
 }
