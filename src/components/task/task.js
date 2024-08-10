2fbb2d4fb749e8d27eefefd361585b4099e15df4
Adam Khashiev <adamkhashiev2004@mail.ru>
2024-08-10 18:33:35 +0300

    task with hooks

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -1,100 +1,97 @@
-import { Component } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import { formatDistanceToNow, format } from 'date-fns'
 import './task.css'
 
-export default class Task extends Component {
-  constructor(props) {
-    super(props)
-    const { label, timerMillisec } = props
-    this.state = {
-      label,
-      timerMillisec,
-      isRunning: false,
-      msPlus: timerMillisec ? -1000 : 1000,
-    }
+export default function Task({
+  id,
+  label,
+  created,
+  onDeleteClick,
+  onEditClick,
+  changeLabel,
+  onCheckClick,
+  completed,
+  editing,
+  timerMillisec,
+  visible,
+}) {
+  const [labelState, setLabel] = useState(label)
+  const [timerMillisecState, setTimerMillisec] = useState(timerMillisec)
+  const [isRunning, setIsRunning] = useState(false)
+  const [msPlus] = useState(timerMillisec ? -1000 : 1000)
+  const intervalIDRef = useRef(null)
+
+  const pauseTimer = () => {
+    setIsRunning(false)
+    clearInterval(intervalIDRef.current)
   }
 
-  componentDidUpdate() {
-    const { completed } = this.props
-    const { isRunning } = this.state
+  useEffect(() => {
     if (completed && isRunning) {
-      this.pauseTimer()
+      pauseTimer()
+    }
+    return () => clearInterval(intervalIDRef.current)
+  }, [completed])
+
+  const updateTimer = (ms) => {
+    if (ms < 0) {
+      clearInterval(intervalIDRef.current)
+      onCheckClick()
+    } else {
+      setTimerMillisec(ms)
     }
   }
 
-  componentWillUnmount() {
-    clearInterval(this.interval)
-  }
-
-  startTimer = () => {
-    const { isRunning } = this.state
-    if (!isRunning) {
-      this.setState({ isRunning: true })
-      this.interval = setInterval(() => {
-        const { timerMillisec, msPlus } = this.state
-        this.updateTimer(timerMillisec + msPlus)
+  const startTimer = () => {
+    if (!isRunning && !completed) {
+      setIsRunning(true)
+      intervalIDRef.current = setInterval(() => {
+        setTimerMillisec((prevMs) => {
+          const newMs = prevMs + msPlus
+          updateTimer(newMs)
+          return newMs
+        })
       }, 1000)
     }
   }
 
-  pauseTimer = () => {
-    this.setState({ isRunning: false })
-    clearInterval(this.interval)
-  }
-
-  updateTimer = (ms) => {
-    if (ms < 0) {
-      clearInterval(this.interval)
-      const { onCheckClick } = this.props
-      onCheckClick()
-    } else {
-      this.setState({ timerMillisec: ms })
-    }
-  }
-
-  onSubmitHandler = (e) => {
+  const onSubmitHandler = (e) => {
     e.preventDefault()
-    const { label } = this.state
-    const { changeLabel } = this.props
-    changeLabel(label)
+    changeLabel(labelState)
   }
 
-  inputChangeHandler = (e) => {
-    this.setState({ label: e.target.value })
+  const inputChangeHandler = (e) => {
+    setLabel(e.target.value)
   }
 
-  render() {
-    const { id, created, onDeleteClick, onEditClick, onCheckClick, completed, editing, visible } = this.props
-    const { label, timerMillisec } = this.state
-    const timeAgo = formatDistanceToNow(created, { includeSeconds: true })
-    const timer = format(new Date(timerMillisec), 'mm:ss')
+  const timeAgo = formatDistanceToNow(created, { includeSeconds: true })
+  const timer = format(new Date(timerMillisecState), 'mm:ss')
 
-    return (
-      <li className={`${completed && 'completed'} ${editing && 'editing'} ${!visible && 'hidden'}`}>
-        <div className="view">
-          <input id={id} className="toggle" type="checkbox" checked={completed} onChange={onCheckClick} />
-          <label htmlFor={id}>
-            <span className="title">{label}</span>
-            <span className="description">
-              <button type="button" aria-label="start" className="icon icon-play" onClick={this.startTimer} />
-              <button type="button" aria-label="stop" className="icon icon-pause" onClick={this.pauseTimer} />
-              {timer}
-            </span>
-            <span className="description">created {timeAgo} ago</span>
-          </label>
-          <button type="button" className="icon icon-edit" aria-label="edit" onClick={onEditClick} />
-          <button type="button" className="icon icon-destroy" aria-label="destroy" onClick={onDeleteClick} />
-        </div>
+  return (
+    <li className={`${completed && 'completed'} ${editing && 'editing'} ${!visible && 'hidden'}`}>
+      <div className="view">
+        <input id={id} className="toggle" type="checkbox" checked={completed} onChange={onCheckClick} />
+        <label htmlFor={id}>
+          <span className="title">{labelState}</span>
+          <span className="description">
+            <button type="button" aria-label="start" className="icon icon-play" onClick={startTimer} />
+            <button type="button" aria-label="stop" className="icon icon-pause" onClick={pauseTimer} />
+            {timer}
+          </span>
+          <span className="description">created {timeAgo} ago</span>
+        </label>
+        <button type="button" className="icon icon-edit" aria-label="edit" onClick={onEditClick} />
+        <button type="button" className="icon icon-destroy" aria-label="destroy" onClick={onDeleteClick} />
+      </div>
 
-        {editing && (
-          <form onSubmit={(e) => this.onSubmitHandler(e)}>
-            <input type="text" className="edit" value={label} onChange={this.inputChangeHandler} />
-          </form>
-        )}
-      </li>
-    )
-  }
+      {editing && (
+        <form onSubmit={(e) => onSubmitHandler(e)}>
+          <input type="text" className="edit" value={labelState} onChange={inputChangeHandler} />
+        </form>
+      )}
+    </li>
+  )
 }
 
 Task.defaultProps = {
